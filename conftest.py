ff58a1717ff4dbc3466b5e38eb658773e963544d
Ari Rizzitano <arizzitano@edx.org>
2017-12-01 16:57:22 -0500

    pytest monkeypatch fix

--- conftest.py
+++ conftest.py
@@ -8,3 +8,12 @@ from __future__ import absolute_import, unicode_literals
 # avoid duplicating the implementation
 
 from cms.conftest import _django_clear_site_cache, pytest_configure  # pylint: disable=unused-import
+
+import pytest
+
+@pytest.fixture(autouse=True)
+def no_webpack_loader(monkeypatch):
+    monkeypatch.setattr(
+        "webpack_loader.templatetags.webpack_loader.render_bundle",
+        lambda x: ''
+    )
